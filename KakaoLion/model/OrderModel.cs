5fda321f1d1131f968062fce0b231283fcde5786
KimJinHo6831 <52987098+KimJinHo6831@users.noreply.github.com>
2020-10-06 10:59:48 +0900

    [ADD] OrderPage

---
+++ KakaoLion/model/OrderModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KakaoLion.model
+{
+    class OrderModel
+    {
+
+        public int idx { get; set; } //주문 목록 인덱스
+        public int menuindx { get; set; } //메뉴 인덱스 참조
+        public int quantity { get; set; } // 수량
+        public int totalPrice { get; set; } //총가격
+        public String userId { get; set; } //userId
+        public String purchaseAt { get; set; } //구매 시간
+        public Boolean paymentPlace { get; set; } // 구매 장소
+        public Boolean paymentMethod { get; set; } //구매 방법
+        public int shopIdx { get; set; } // 지점 선택시 
+
+    }
+}
