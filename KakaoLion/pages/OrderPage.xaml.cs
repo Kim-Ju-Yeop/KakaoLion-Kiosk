6f3eee51009eee4f0fadbf4a981ab8f670bc86c4
KimJinHo6831 <52987098+KimJinHo6831@users.noreply.github.com>
2020-10-16 08:49:42 +0900

    [FIX] OrderPage

--- KakaoLion/pages/OrderPage.xaml.cs
+++ KakaoLion/pages/OrderPage.xaml.cs
@@ -13,13 +13,6 @@ namespace KakaoLion.pages
 { 
     public partial class OrderPage : Page
     {
-        /*private List<MenuModel> _menuList = new List<MenuModel>();
-        public List<MenuModel> MenuList 
-        {
-            get => _menuList;
-            set => _menuList = value;
-        }
-*/
         public int pageCount = 1;
         public List<MenuModel> MenuList = new List<MenuModel>()
         {
@@ -29,7 +22,6 @@ namespace KakaoLion.pages
                 name="라이언 치즈볼 쿠션 팩",
                 price = 12000,
                 discount = 0,
-                page = 1,
                 imagePath = "/resources/image/small/라이언 치즈볼 쿠션 팩.jpg"
             },
             new MenuModel() {
@@ -38,7 +30,6 @@ namespace KakaoLion.pages
                 name="레몬 테라스 미니키체인 라이언",
                 price = 12000,
                 discount = 0,
-                page = 1,
                 imagePath = "/resources/image/small/레몬 테라스 미니키체인 라이언.jpg"
             },
             new MenuModel() {
@@ -47,7 +38,6 @@ namespace KakaoLion.pages
                 name="리본 라이언",
                 price = 12000,
                 discount = 0,
-                page = 1,
                 imagePath = "/resources/image/small/리본 라이언.jpg"
             },
             new MenuModel() {
@@ -56,7 +46,6 @@ namespace KakaoLion.pages
                 name="리틀 프렌즈 핑거퍼펫 세트 v1",
                 price = 12000,
                 discount = 0,
-                page = 1,
                 imagePath = "/resources/image/small/리틀 프렌즈 핑거퍼펫 세트 v1.jpg"
             },
             new MenuModel() {
@@ -65,7 +54,6 @@ namespace KakaoLion.pages
                 name="마린 마스코트 키체인 라이언",
                 price = 12000,
                 discount = 0,
-                page = 1,
                 imagePath = "/resources/image/small/마린 마스코트 키체인 라이언.jpg"
             },
             new MenuModel() {
@@ -74,7 +62,6 @@ namespace KakaoLion.pages
                 name="리틀 프렌즈 핑거퍼펫 세트 v1",
                 price = 12000,
                 discount = 0,
-                page = 1,
                 imagePath = "/resources/image/small/리틀 프렌즈 핑거퍼펫 세트 v1.jpg"
             },
             new MenuModel() {
@@ -83,7 +70,6 @@ namespace KakaoLion.pages
                 name="리틀 프렌즈 핑거퍼펫 세트 v1",
                 price = 12000,
                 discount = 0,
-                page = 1,
                 imagePath = "/resources/image/small/리틀 프렌즈 핑거퍼펫 세트 v1.jpg"
             },
             new MenuModel() {
@@ -92,7 +78,6 @@ namespace KakaoLion.pages
                 name="리틀 프렌즈 핑거퍼펫 세트 v1",
                 price = 12000,
                 discount = 0,
-                page = 1,
                 imagePath = "/resources/image/small/리틀 프렌즈 핑거퍼펫 세트 v1.jpg"
             },
              new MenuModel() {
@@ -101,7 +86,6 @@ namespace KakaoLion.pages
                 name="리틀 프렌즈 핑거퍼펫 세트 v1",
                 price = 12000,
                 discount = 0,
-                page = 1,
                 imagePath = "/resources/image/small/리틀 프렌즈 핑거퍼펫 세트 v1.jpg"
             },
             new MenuModel() {
@@ -110,7 +94,22 @@ namespace KakaoLion.pages
                 name="리틀 프렌즈 핑거퍼펫 세트 v1",
                 price = 12000,
                 discount = 0,
-                page = 2,
+                imagePath = "/resources/image/small/리틀 프렌즈 핑거퍼펫 세트 v1.jpg"
+            },
+            new MenuModel() {
+                idx = 10,
+                category = Category.Small,
+                name="리틀 프렌즈 핑거퍼펫 세트 v1",
+                price = 12000,
+                discount = 0,
+                imagePath = "/resources/image/small/리틀 프렌즈 핑거퍼펫 세트 v1.jpg"
+            },
+            new MenuModel() {
+                idx = 11,
+                category = Category.Small,
+                name="리틀 프렌즈 핑거퍼펫 세트 v1",
+                price = 12000,
+                discount = 0,
                 imagePath = "/resources/image/small/리틀 프렌즈 핑거퍼펫 세트 v1.jpg"
             },
 
@@ -121,7 +120,6 @@ namespace KakaoLion.pages
                 name="25cm인형 치즈볼 라이언",
                 price = 8000,
                 discount = 0,
-                page = 1,
                 imagePath = "/resources/image/medium/25cm인형 치즈볼 라이언.jpg"
             },
             new MenuModel() {
@@ -130,7 +128,6 @@ namespace KakaoLion.pages
                 name="꿀잠 베이비 필로우 라이언",
                 price = 8000,
                 discount = 0,
-                page = 1,
                 imagePath = "/resources/image/medium/꿀잠 베이비 필로우 라이언.jpg"
             },
             new MenuModel() {
@@ -139,7 +136,6 @@ namespace KakaoLion.pages
                 name="레몬 테라스 얼굴 쿠션 라이언",
                 price = 8000,
                 discount = 0,
-                page = 1,
                 imagePath = "/resources/image/medium/레몬 테라스 얼굴 쿠션 라이언.jpg"
             },
             new MenuModel() {
@@ -148,7 +144,6 @@ namespace KakaoLion.pages
                 name="레몬 테라스 향기 인형 라이언",
                 price = 8000,
                 discount = 0,
-                page = 1,
                 imagePath = "/resources/image/medium/레몬 테라스 향기 인형 라이언.jpg"
             },
 
@@ -159,7 +154,6 @@ namespace KakaoLion.pages
                 name="롱 바디 필로우 라이언",
                 price = 5000,
                 discount = 0,
-                page = 1,
                 imagePath = "/resources/image/big/롱 바디 필로우 라이언.jpg"
             },
             new MenuModel() {
@@ -168,7 +162,6 @@ namespace KakaoLion.pages
                 name="리틀 바디 필로우 라이언 인형",
                 price = 5000,
                 discount = 0,
-                page = 1,
                 imagePath = "/resources/image/big/리틀 바디 필로우 라이언 인형.jpg"
             },
             new MenuModel() {
@@ -177,7 +170,6 @@ namespace KakaoLion.pages
                 name="말랑 허그 바디 라이언 쿠션",
                 price = 5000,
                 discount = 0,
-                page = 1,
                 imagePath = "/resources/image/big/말랑 허그 바디 라이언 쿠션.jpg"
             },
             new MenuModel() {
@@ -186,7 +178,6 @@ namespace KakaoLion.pages
                 name="메가 바디 필로우 리틀 라이언",
                 price = 5000,
                 discount = 0,
-                page = 1,
                 imagePath = "/resources/image/big/메가 바디 필로우 리틀 라이언.jpg"
             }
 
@@ -196,15 +187,24 @@ namespace KakaoLion.pages
         {
             InitializeComponent();
             previous.IsEnabled = false;
+            Category category = (Category)lbCategory.SelectedIndex;
+            for (int i = 0; i < MenuList.Count; i++)
+            {
+                if (MenuList[i].idx < 9 )
+                {
+                    MenuList[i].page = 1;
+                }
+                else if (MenuList[i].idx > 8 && MenuList[i].idx < 18)
+                {
+                    MenuList[i].page = 2;
+                }
+            }
             Loaded += OrderPage_Loaded;
         }
 
         private void OrderPage_Loaded(object sender, System.Windows.RoutedEventArgs e)
         {
-            /* DataContext = this;
-             setMenuList();*/
             lbCategory.SelectedIndex = 0;
-
         }
 
         public List<MenuModel> PageChange(int pageCount)
@@ -215,13 +215,12 @@ namespace KakaoLion.pages
 
         private void previous_Click(object sender, RoutedEventArgs e)
         {
-
+            
             --this.pageCount;
             lbMenus.ItemsSource = PageChange(this.pageCount);
-            if (this.pageCount-1 <= 0)
+            if (this.pageCount - 1 <= 0)
                 previous.IsEnabled = false;
             next.IsEnabled = true;
-
         }
 
         private void next_Click(object sender, RoutedEventArgs e)
@@ -230,9 +229,22 @@ namespace KakaoLion.pages
             ++this.pageCount;
             lbMenus.ItemsSource = PageChange(this.pageCount);
             previous.IsEnabled = true;
-            if (this.pageCount+1 > 2)
-                next.IsEnabled = false;
+            for (int i = 0; i < MenuList.Count; i++)
+            {
+                if (this.pageCount == MenuList[i].page)
+                {
+                    next.IsEnabled = false;
+                }
+            }
 
+        }
+       private void lbMenus_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MenuModel menu = (MenuModel)lbMenus.ItemsSource;
+            OrderModel orderMenu = (OrderModel)lbMenus.ItemsSource;
+            orderMenu.menuindx = menu.idx;
+            orderMenu.quantity = 1;
+            orderMenu.totalPrice = (orderMenu.quantity) * (menu.price);
         }
 
         private void lbCategory_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -243,91 +255,6 @@ namespace KakaoLion.pages
             lbMenus.ItemsSource = PageChange(this.pageCount);
         }
 
-       
-        /* public void setMenuList()
-         {
-            *//* MenuModel menuModel1 = new MenuModel();
-             menuModel1.idx = 0;
-             menuModel1.category = 1;
-             menuModel1.name = "라이언 치즈볼 쿠션 팩";
-             menuModel1.price = 12000;
-             menuModel1.discount = 0;
-             menuModel1.imagePath = "/resources/image/small/라이언 치즈볼 쿠션 팩.jpg";
-
-             MenuModel menuModel2 = new MenuModel();
-             menuModel2.idx = 1;
-             menuModel2.category = 1;
-             menuModel2.name = "레몬 테라스 미니키체인 라이언";
-             menuModel2.price = 12000;
-             menuModel2.discount = 0;
-             menuModel2.imagePath = "/resources/image/small/레몬 테라스 미니키체인 라이언.jpg";
-
-             MenuModel menuModel3 = new MenuModel();
-             menuModel3.idx = 2;
-             menuModel3.category = 1;
-             menuModel3.name = "리본 라이언";
-             menuModel3.price = 12000;
-             menuModel3.discount = 0;
-             menuModel3.imagePath = "/resources/image/small/리본 라이언.jpg";
-
-             MenuModel menuModel4 = new MenuModel();
-             menuModel4.idx = 3;
-             menuModel4.category = 1;
-             menuModel4.name = "리틀 프렌즈 핑거퍼펫 세트 v1";
-             menuModel4.price = 12000;
-             menuModel4.discount = 0;
-             menuModel4.imagePath = "/resources/image/small/리틀 프렌즈 핑거퍼펫 세트 v1.jpg";
-
-             MenuModel menuModel5 = new MenuModel();
-             menuModel5.idx = 4;
-             menuModel5.category = 1;
-             menuModel5.name = "마린 마스코트 키체인 라이언";
-             menuModel5.price = 12000;
-             menuModel5.discount = 0;
-             menuModel5.imagePath = "/resources/image/small/마린 마스코트 키체인 라이언.jpg";
-
-             MenuModel menuModel6 = new MenuModel();
-             menuModel6.idx = 5;
-             menuModel6.category = 1;
-             menuModel6.name = "마린 페이스 키체인 라이언";
-             menuModel6.price = 12000;
-             menuModel6.discount = 0;
-             menuModel6.imagePath = "/resources/image/small/마린 페이스 키체인 라이언.jpg";
-
-             MenuModel menuModel7 = new MenuModel();
-             menuModel7.idx = 6;
-             menuModel7.category = 1;
-             menuModel7.name = "미니 라이언 위드 아이앱 스튜디오";
-             menuModel7.price = 12000;
-             menuModel7.discount = 0;
-             menuModel7.imagePath = "/resources/image/small/미니 라이언 위드 아이앱 스튜디오.jpg";
-
-             MenuModel menuModel8 = new MenuModel();
-             menuModel8.idx = 7;
-             menuModel8.category = 1;
-             menuModel8.name = "미니 인형 플라밍고 라이언";
-             menuModel8.price = 12000;
-             menuModel8.discount = 0;
-             menuModel8.imagePath = "/resources/image/small/미니 인형 플라밍고 라이언.jpg";
-
-             MenuModel menuModel9 = new MenuModel();
-             menuModel9.idx = 8;
-             menuModel9.category = 1;
-             menuModel9.name = "미니페이스 리틀라이언";
-             menuModel9.price = 12000;
-             menuModel9.discount = 0;
-             menuModel9.imagePath = "/resources/image/small/미니페이스 리틀라이언.jpg";
-
-             MenuList.Add(menuModel1);
-             MenuList.Add(menuModel2);
-             MenuList.Add(menuModel3);
-             MenuList.Add(menuModel4);
-             MenuList.Add(menuModel5);
-             MenuList.Add(menuModel6);
-             MenuList.Add(menuModel7);
-             MenuList.Add(menuModel8);
-             MenuList.Add(menuModel9);*//*
-         }*/
-
+        
     }
 }
