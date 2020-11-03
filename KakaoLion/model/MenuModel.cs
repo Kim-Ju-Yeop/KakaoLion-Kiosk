ca84bd5a363c99eadf5ad6d8364208bc955a4b43
KimJinHo6831 <52987098+KimJinHo6831@users.noreply.github.com>
2020-11-03 11:21:26 +0900

    [Add] OrderPage discount

--- KakaoLion/model/MenuModel.cs
+++ KakaoLion/model/MenuModel.cs
@@ -8,9 +8,9 @@ namespace KakaoLion.model
         public int idx { get; set; }
         public int page { get; set; }
         public Category category { get; set; }
-        public String name { get; set; }
+        public string name { get; set; }
         public int price { get; set; }
         public int discount { get; set; }
-        public String imagePath { get; set; }
+        public string imagePath { get; set; }
     }
 }
