1f44528d6442815f4b785c718d9db9359bceb3a0
jundogown <ns8708ns@naver.com>
2020-11-04 13:56:21 +0900

    [ADD] Login

---
+++ KakaoLion/LoginWIndow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using KakaoLion.model;
+using KakaoLion.widget;
+using MySql.Data.MySqlClient;
+
+namespace KakaoLion.pages
+{
+    public partial class loginWIndow : Window
+    {
+        private static List<UserModel> userList = new List<UserModel>();
+
+        public loginWIndow()
+        {
+            InitializeComponent();
+            Loaded += LoginWIndow_Loaded;
+        }
+
+        private void LoginWIndow_Loaded(object sender, RoutedEventArgs e)
+        {
+            getUserInfo();
+        }
+
+        private void getUserInfo()
+        {
+            using (MySqlConnection conn = new MySqlConnection(Constants.CONNSTR))
+            {
+                conn.Open();
+                string sql = "SELECT * FROM lion.user";
+
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                { 
+                    userList.Add(new UserModel
+                    {
+                        id = (string)rdr["id"],
+                        pw = (string)rdr["pw"],
+                        name = (string)rdr["name"],
+                        address = (string)rdr["address"],
+                        barcode = (string)rdr["barcode"],
+                        qrcode = (string)rdr["qrcode"],
+                    });
+                }
+            }
+        }
+
+        private void button1_Click_1(object sender, RoutedEventArgs e)
+        {
+            foreach (UserModel u in userList)
+            {
+                
+
+                if (u.id != textBoxId.Text.ToString() || u.pw != textBoxPw.Text.ToString())
+                {
+                    MessageBox.Show("로그인 정보가 올바르지 않습니다.", "KAKAO");
+                } else
+                {
+                    var result = MessageBox.Show("로그인 성공 :)", "KAKAO");
+                    if (result == MessageBoxResult.OK)
+                    {
+
+                        MainWindow MainWindow = new MainWindow();
+                        MainWindow.Show();
+                        this.Close();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ KakaoLion/model/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KakaoLion.model
+{
+    class UserModel
+    {
+        public string id { get; set; }
+        public string pw { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public string barcode { get; set; }
+        public string qrcode { get; set; }
+    }
+}
