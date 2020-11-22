9094380e1b201d8d189700985fee3795eb8eec4d
김주엽 <49600974+Kim-Ju-Yeop@users.noreply.github.com>
2020-11-22 21:19:33 +0900

    [ADD] Server Disconnected
    서버 연결이 유실되었을 경우 처리했습니다.

--- KakaoLion/widget/Constants.cs
+++ KakaoLion/widget/Constants.cs
@@ -9,7 +9,7 @@ namespace KakaoLion.widget
     class Constants
     {
         public const string DATABASE_CONNSTR = "Server=10.80.161.125;Database=lion;Uid=juyeop;Pwd=9946";
-        public const string SERVER_CONNSTR = "10.80.163.141";
+        public const string SERVER_CONNSTR = "10.80.162.228";
         public const int PORT = 80;
     }
 }
