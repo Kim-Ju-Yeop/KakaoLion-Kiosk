5405996dc60453ab3ebd02dd5b5a868fe48234ca
김주엽 <49600974+Kim-Ju-Yeop@users.noreply.github.com>
2020-11-30 16:43:18 +0900

    [ADD] HomePage
    HomePage Discription

--- KakaoLion/widget/Constants.cs
+++ KakaoLion/widget/Constants.cs
@@ -9,7 +9,7 @@ namespace KakaoLion.widget
     class Constants
     {
         public const string DATABASE_CONNSTR = "Server=10.80.161.125;Database=lion;Uid=juyeop;Pwd=9946";
-        public const string SERVER_CONNSTR = "10.80.161.176";
+        public const string SERVER_CONNSTR = "10.80.163.141";
         public const int PORT = 80;
     }
 }
